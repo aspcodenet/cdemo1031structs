0884bd8f0cd97147cf00dd2248a3c8d85beec039
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-10-31 09:46:15 +0100

    a

--- main.c
+++ main.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "inputhelpers.h"
+
+typedef struct {
+    int Age;
+    int Jersey;
+    char Name[256];
+}Player;
 
 void showMenu();
 
+// PEKARE av två anledningar
+// 1. möjliggöra ändring i en parameter (copy by value)
+// 2. prestandaskäl 4 bytes för pekaren istf hela objektet är kopierat
+void printPlayer(Player *p){ // värdet av p är 1000
+    printf("*** INFO ABOUT PLAYER ***\n");
+    printf("%d, %s\n", p->Jersey, p->Name);
+    //p->Jersey = 99;
+}
+
 int main() { 
+    Player foppa; // foppa är paxad på STACKENB på address 1000 -> 1264
+    foppa.Age = 52;
+    foppa.Jersey = 21;
+    strcpy(foppa.Name, "Peter Forsberg");
+    printPlayer(&foppa); // Det är 1000 som skickas in
+    // //foppa.Name = "Peter Forsberg";
+    // printf("%s\n", foppa.Name);
+
+    // Player patrik = {55,14,"Patrik Sundström"};
+    // printf("%s\n", patrik.Name);
+ 
+
+
     while(true){
         showMenu();
         int selection = getMenuInput(1,4);
